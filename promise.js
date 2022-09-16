1d8bc8e637ac8eda9014f342821d233ad371b98e
@daudfauzy98 <daudfauzy@gmail.com>
2022-09-16 10:42:35 +0700

    Create async function to calculate movies emotion result

--- promise.js
+++ promise.js
@@ -1,7 +1,31 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+   let theaterIXX = await promiseTheaterIXX()
+   let theaterVGC = await promiseTheaterVGC()
+
+   return new Promise ((resolve, reject) => {
+      if (theaterIXX && theaterVGC) {
+         let countEmotions = 0
+
+         for (films of theaterIXX) {
+            if (films.hasil === emosi) {
+               countEmotions += 1
+            }
+         }
+         for (films of theaterVGC) {
+            if (films.hasil === emosi) {
+               countEmotions += 1
+            }
+         }
+
+         resolve(countEmotions)
+      } else {
+         reject("Data kosong atau terjadi error")
+      }
+   })
+};
 
 module.exports = {
   promiseOutput,
